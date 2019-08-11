db11f510a408aa5fe86916780911421ccb2a83ae
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-11 02:41:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,12 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
+      if item.nil?
+        resp.write "Item not found"
+        resp.status = 400
+      else
       resp.write item.price
+    ends
   else
     resp.write "Route not found"
     resp.status = 404
