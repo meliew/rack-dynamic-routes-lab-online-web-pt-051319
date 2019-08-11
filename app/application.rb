362ff66fb9b9317b1d190b8dc81e4a488320fabc
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-11 02:41:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,11 @@
 class Application
-
+  
   @@items = []
-
+  
   def call(env)
     resp= Rack::Response.new
     req = Rack::Request.new(env)
-
+    
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
@@ -13,13 +13,13 @@ class Application
         resp.write "Item not found"
         resp.status = 400
       else
-      resp.write item.price
+        resp.write item.price
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
     end
-  else
-    resp.write "Route not found"
-    resp.status = 404
-  end
-
-  resp.finish
+    
+    resp.finish
   end
 end
